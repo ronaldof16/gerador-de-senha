ffbb918d5484d01a2ad354396974836c02c82f5b
ronaldof16 <ronaldof16@hotmail.com>
2022-12-29 23:25:19 -0300

    Criação das variáveis no javascript

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+let containerPassword = document.querySelector("#container-password");
+let charset = "abcdefghikjlmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+let novaSenha = "";
+
+//sizePassword.innerHTML = sliderElement.value;
